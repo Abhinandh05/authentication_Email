01d7a40b333b5a9fc95e77c36c3311e450c42d9a
Abhinandhc <abhinandhc407@gmail.com>
2025-02-05 22:08:03 +0530

    logout controller complete

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,7 +32,7 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
-        return res.json({ message: "Registration successful", success: true });
+        return res.json({ message: "Registration successful", success: true }); 
 
     } catch (error) {
         res.status(400).json({ message: error.message, success: false });
@@ -77,4 +77,22 @@ export const login = async (req, res) => {
     } catch (error) {
         res.status(400).json({ message: error.message, success: false });
     }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production",
+            'none': 'strict',
+
+        })
+        return res.json({ message: "Logged out successfully", success: true });
+        
+    } catch (error) {
+        res.status(400).json({ message: error.message, success: false });
+    }
+   
+
 }

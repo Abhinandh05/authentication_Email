9e7b351fdb95b9dfa945708f21b9c4df7f3d29f8
Abhinandhc <abhinandhc407@gmail.com>
2025-02-05 22:01:46 +0530

    loginPartCompleted

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,39 +3,76 @@ import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 
 export const register = async (req, res) => {
-    
+
 
     const { name, email, password } = req.body;
 
-    if(!name || !email || !password){
+    if (!name || !email || !password) {
         return res.status(400).json({ message: "All fields are required", success: false });
     }
 
     try {
-     const existingUser = await userModel.findOne({ email });
+        const existingUser = await userModel.findOne({ email });
 
-     if(existingUser){
-         return res.status(400).json({ message: "Email already exists", success: false });
-     }
-     const hashedPassword = await bcrypt.hash(password, 10);
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already exists", success: false });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-     const user = new userModel({name, email, password: hashedPassword});
-     await user.save();
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
 
-     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-     res.cookie('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production",
-        'none':'strict',
-        
-        maxAge: 7 * 24 * 60 * 60 * 1000
-     } )
-        
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production",
+            'none': 'strict',
+
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
     } catch (error) {
-       res.status(400).json({ message: error.message, success: false }); 
+        res.status(400).json({ message: error.message, success: false });
     }
 
-        
 
+
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required", success: false });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User does not exist", success: false });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials", success: false });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production",
+            'none': 'strict',
+
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.json({ message: "Login successful", success: true });
+
+    } catch (error) {
+        res.status(400).json({ message: error.message, success: false });
+    }
 }

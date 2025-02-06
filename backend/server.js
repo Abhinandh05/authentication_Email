4f0701ae41efd46161f91104b75d716b0c60de84
Abhinandhc <abhinandhc407@gmail.com>
2025-02-06 20:31:56 +0530

    auth route is created

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,31 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Connect to MongoDB
 connectDB();
 
-
-
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    credentials: true,}));
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+}));
 
-
-
-
+// API Endpoints
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);   
+
+// Authentication Routes (Register, Login, Logout)
+app.use("/api/auth", authRouter);
+
+// Start the Server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
